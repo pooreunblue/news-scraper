e2a52d906d8aa8acb4128c396081417672ac1fa1
bsh <mjbsh02@mju.ac.kr>
2026-06-25 14:54:33 +0900

    fix : 콘솔 입력 처리 보완

--- src/oop/search/presentation/ConsoleNewsApp.java
+++ src/oop/search/presentation/ConsoleNewsApp.java
@@ -16,13 +16,14 @@ public class ConsoleNewsApp {
         Scanner sc = new Scanner(System.in);
         while (true) { // 예외처리는 생략
             System.out.println("[검색할 키워드를 입력해주세요 (q 입력 시 종료)]");
-            String keyword = sc.nextLine();
+            String keyword = sc.nextLine().trim(); // 스페이스 문제 제거
             if (keyword.equals("q")) {
                 System.out.println("[검색을 종료합니다]");
                 break;
             }
             System.out.println("[몇 건 검색하시겠습니까? (양의 정수)]");
             int limit = sc.nextInt();
+            sc.nextLine(); // 버퍼 비우기를 안하면 이후 다음 nextLine 호출 시 엔터키 입력으로 인한 빈 문자열 입력
             newsService.search(keyword, limit);
             System.out.println("[검색이 완료 되었습니다]");
         }
